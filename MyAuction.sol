87578f2646dedfe85b0828b5d794d1c5f1291838
ahmedcs1995 <ahmedcs1995@gmail.com>
2019-03-20 00:54:25 +0500

    Update MyAuction.sol

--- MyAuction.sol
+++ MyAuction.sol
@@ -1,126 +1,120 @@
-pragma solidity >=0.4.22 < 0.6.0;
+pragma solidity ^0.5.0;
 
-contract MyAuction{
+contract AuctionContract {
 
-    //static 
-    address public owner;
-    
-    
-    //timestamps
-    uint public auctionEndTime;
-    
-    // Current state of the auction.
-    address public highestBidder;
-    uint public highestBid;
-    
-    
-     // Allowed withdrawals of previous bids
-    mapping(address => uint) pendingReturns;
-    
-    // Set to true at the end, disallows any change.
-    // By default initialized to `false`.
-    bool ended;
-      
-   
-    bool cancel;
-    
-    constructor(uint _biddingTime,address _owner,uint initialAmount) public {
-        owner = _owner;
-        auctionEndTime = now + _biddingTime;
-        highestBid = initialAmount;
+// Model a Auction
+   struct Auction {
+      uint id;
+      string auctionName;
+      address payable beneficiary;
+      address payable highestBidder;
+      uint highestBid;
+      bool ended;
+      bool isDelivered;
+      string firebaseHash;
+   }
+
+
+   // Events that will be emitted on changes.
+   event HighestBidIncreased(address bidder, uint amount);
+   event AuctionEnded(address winner, uint amount);
+   event SuccessfulDelivered(address winner, uint amount);
+
+
+   // Store auctions Count
+  uint public auctionsCount;
+
+
+      // Read/write Auctions
+   mapping(uint => Auction) public auctions;
+
+
+    constructor() public  {
     }
-    
-    
-    
-    function placeBid() public payable onlyNotOwner onlyBeforeEnd highestBidCheck{
-        
-        
-         if (highestBid != 0) {
-            // Sending back the money by simply using
-            // highestBidder.send(highestBid) is a security risk
-            // because it could execute an untrusted contract.
-            // It is always safer to let the recipients
-            // withdraw their money themselves.
-            pendingReturns[highestBidder] += highestBid;
-        }
-        
-        highestBidder = msg.sender;
-        highestBid = msg.value;
-    }
-    
-    //owner can't bid 
-     modifier onlyNotOwner { require(msg.sender == owner,"Owner cannot bid the auction"); _; }
-     
-     // for cancel auction
-     modifier onlyOwner  { require(msg.sender != owner); _; }
-     
-     
-      // for cancel auction
-     modifier onlyNotCanceled  { require(cancel); _; }
-     
-     
-     //check period is over.
-     modifier onlyBeforeEnd { require(now <= auctionEndTime,"Auction already ended."); _; }
-     
-     
-     
-      //check higher Bid.
-     modifier highestBidCheck { require(msg.value > highestBid,"There already is a higher bid."); _; }
-    
-    
- 
-     
-    
-    // Withdraw a bid that was overbid.
-      function withdraw() public returns (bool) {
-        uint amount = pendingReturns[msg.sender];
-        if (amount > 0) {
-            // It is important to set this to zero because the recipient
-            // can call this function again as part of the receiving call
-            // before `send` returns.
-            pendingReturns[msg.sender] = 0;
 
-            if (!msg.sender.send(amount)) {
-                // No need to call throw here, just reset the amount owing
-                pendingReturns[msg.sender] = amount;
-                return false;
-            }
-        }
-        return true;
-    } 
-    
-     // End the auction and send the highest bid
-    // to the beneficiary.
-    function auctionEnd() public {
-       // 1. Conditions
-        require(now >= auctionEndTime, "Auction not yet ended.");
-        require(!ended, "auctionEnd has already been called.");
+   function createAuction (string memory _name,string memory firebaseHash,uint minAmount) payable public{
+       auctionsCount++;
+       auctions[auctionsCount] = Auction(auctionsCount,_name, msg.sender,msg.sender,minAmount,false,false,firebaseHash);
+   }
 
-        // 2. Effects
-        ended = true;
-      
+   function findWinner(uint _id) view public returns (address) {
+       Auction memory auction = auctions[_id];
 
-        // 3. Interaction
-        owner.transfer(highestBid);
-    }
-     
-  function cancelAuction()
-        onlyOwner
-        onlyBeforeEnd
-        onlyNotCanceled
-        returns (bool success)
-    {
-        cancel = true;
-        return true;
-    }
-     
-     
-     
-     
-     
-     
-     
-     
-     
-     
+
+      require(auction.ended,"Auction is not ended yet.");
+
+      return auction.highestBidder;
+   }
+
+   function bid(uint _id) public payable {
+       Auction memory auction = auctions[_id];
+
+
+       require(auction.beneficiary != msg.sender,"Owner cannot Bid");
+
+       require(!auction.ended,"Auction already ended.");
+
+       // If the bid is not higher, send the
+       // money back.
+
+       //require(auction.beneficiary != auction.highestBidder && msg.value > auction.highestBid,"Bid Must be greater than Min");
+
+       require(msg.value > auction.highestBid,"There already is a higher bid.");
+
+       if (auction.highestBid != 0 && auction.beneficiary != auction.highestBidder) {
+           auction.highestBidder.transfer(auction.highestBid);
+       }
+       auction.highestBidder = msg.sender;
+       auction.highestBid = msg.value;
+
+
+      auctions[_id] = auction;
+
+       emit HighestBidIncreased(msg.sender, msg.value);
+   }
+
+  function auctionEnd(uint _id) public {
+      Auction memory auction = auctions[_id];
+
+      // 1. Conditions
+      require(!auction.ended, "auctionEnd has already been called.");
+
+      require(msg.sender == auction.beneficiary, "Only owner can end this Auction");
+
+
+      // 2. Effects
+      auction.ended = true;
+      emit AuctionEnded(auction.highestBidder, auction.highestBid);
+
+      // 3. Interaction
+      auction.beneficiary.transfer(auction.highestBid/2);
+
+      auctions[_id] = auction;
+  }
+  function verifyDelivery(uint _id) public payable{
+      Auction memory auction = auctions[_id];
+
+      require(msg.sender == auction.highestBidder , "Only winner can execute this");
+
+      auction.beneficiary.transfer(auction.highestBid/2);
+
+      auction.isDelivered = true;
+
+
+      emit SuccessfulDelivered(auction.highestBidder, auction.highestBid);
+
+      auctions[_id] = auction;
+  }
+
+  function editAuction(uint _id,string memory _name) public {
+    Auction memory auction = auctions[_id];
+
+    //conditions
+    require(msg.sender == auction.beneficiary, "Only owner can edit this Auction");
+
+    //update
+    auction.auctionName = _name;
+
+    auctions[_id] = auction;
+  }
 }
